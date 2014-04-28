b6f436a385da7d8646348ca0e9f1047b7afa9167
Nuno Morais <geral@nuno-morais.eu>
2014-04-28 09:54:15 +0100

    Adicionadas as tabelas de Etapas, Ultimos Trabalhos e ultimas notas e comentários ao aluno

--- app/controllers/DashboardStudent.java
+++ app/controllers/DashboardStudent.java
@@ -2,6 +2,8 @@ package controllers;
 
 import models.Discipline;
 import models.Milestone;
+import models.Project;
+import models.StudentMilestone;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
@@ -14,13 +16,16 @@ public class DashboardStudent extends Controller {
 
     /**
      * Display the Dashboard
+     *
      * @return
      */
     public static Result index() {
         return ok(
                 views.html.Dashboard.dashboard.render(
-                    Discipline.findByStudent(request().username()),
-                    Milestone.findByStudent(request().username())
+                        Discipline.findByStudent(request().username()),
+                        Milestone.findByStudent(request().username()),
+                        Project.getByCreatedDate(request().username()),
+                        StudentMilestone.getLastAvaliations(request().username())
                 )
         );
     }
