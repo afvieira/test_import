e683c6faaa8d50869cefd6f89a72678f2c6d37dd
Nuno Morais <geral@nuno-morais.eu>
2014-05-01 14:03:23 +0100

    Adicionado método para fazer roteamento sem replicar código. Controller Dashboards a encaminhar para cada um dos controladores

--- app/controllers/Dashboards.java
+++ app/controllers/Dashboards.java
@@ -18,17 +18,32 @@ public class Dashboards extends Controller {
      */
     @Security.Authenticated(Secured.class)
     public static Result index() {
+        return routing(
+                DashboardStudent.index(),
+                DashboardTeacher.index(),
+                TODO);
+    }
+
+    /**
+     * Método para não duplicar código em cada um dos Métodos
+     * @param callMethodStudent
+     * @param callMethodTeacher
+     * @param callMethodAdmininstrator
+     * @return Result
+     */
+    private static Result routing(Result callMethodStudent, Result callMethodTeacher, Result callMethodAdmininstrator){
         Result result = null;
         User u = User.findByEmail(request().username());
         if(u != null){
             switch(u.userType){
                 case "Teacher":
-                    result = DashboardTeacher.index();
+                    result = callMethodTeacher;
                     break;
                 case "Student":
-                    result =  DashboardStudent.index();
+                    result =  callMethodStudent;
                     break;
                 case "Administrator":
+                    result =  callMethodAdmininstrator;
                     break;
                 default:
                     // TODO: Aqui certamente temos que mandar para uma página de erro!!! (nunca deverá acontecer)
@@ -38,154 +53,133 @@ public class Dashboards extends Controller {
         return result;
     }
 
-
     @Security.Authenticated(Secured.class)
     public static Result disciplines() {
-        Result result = null;
-        User u = User.findByEmail(request().username());
-        if(u != null){
-            switch(u.userType){
-                case "Teacher":
-                    result =  DashboardTeacher.disciplines();
-                    break;
-                case "Student":
-                    result =  DashboardStudent.disciplines();
-                    break;
-                case "Administrator":
-                    break;
-                default:
-                    // TODO: Aqui certamente temos que mandar para uma página de erro!!! (nunca deverá acontecer)
-                    result = ok("BUUMMM");
-            }
-        }
-        return result;
+        return routing(
+                DashboardStudent.disciplines(),
+                DashboardTeacher.disciplines(),
+                TODO);
     }
 
     @Security.Authenticated(Secured.class)
     public static Result showDiscipline(Long id) {
-        return TODO;
+        return routing(
+                DashboardStudent.showDiscipline(id),
+                DashboardTeacher.showDiscipline(id),
+                TODO);
     }
 
     @Security.Authenticated(Secured.class)
     public static Result createDiscipline() {
-        return TODO;
+        return routing(
+                TODO,
+                TODO,
+                TODO);
     }
 
     @Security.Authenticated(Secured.class)
     public static Result deleteDiscipline(Long id) {
-        return TODO;
+        return routing(
+                TODO,
+                TODO,
+                TODO);
     }
 
     @Security.Authenticated(Secured.class)
     public static Result projects() {
-        Result result = null;
-        User u = User.findByEmail(request().username());
-        if(u != null){
-            switch(u.userType){
-                case "Teacher":
-                    result =  DashboardTeacher.projects();
-                    break;
-                case "Student":
-                    result =  DashboardStudent.projects();
-                    break;
-                case "Administrator":
-                    break;
-                default:
-                    // TODO: Aqui certamente temos que mandar para uma página de erro!!! (nunca deverá acontecer)
-                    result = ok("BUUMMM");
-            }
-        }
-        return result;
+        return routing(
+                DashboardStudent.projects(),
+                DashboardTeacher.projects(),
+                TODO);
     }
 
 
     @Security.Authenticated(Secured.class)
     public static Result showProject(Long id) {
-        return TODO;
+        return routing(
+                DashboardStudent.showProject(id),
+                DashboardTeacher.showProject(id),
+                TODO);
     }
 
     @Security.Authenticated(Secured.class)
     public static Result createProject() {
-        return TODO;
+        return routing(
+                DashboardStudent.createProject(),
+                DashboardTeacher.createProject(),
+                TODO);
     }
 
     @Security.Authenticated(Secured.class)
     public static Result deleteProject(Long id) {
-        return TODO;
+        return routing(
+                DashboardStudent.deleteProject(id),
+                DashboardTeacher.deleteProject(id),
+                TODO);
     }
 
     @Security.Authenticated(Secured.class)
     public static Result milestones(Long id_project) {
-        Result result = null;
-        User u = User.findByEmail(request().username());
-        if(u != null){
-            switch(u.userType){
-                case "Teacher":
-                    result =  DashboardTeacher.milestones(id_project);
-                    break;
-                case "Student":
-                    result =  DashboardStudent.milestones(id_project);
-                    break;
-                case "Administrator":
-                    break;
-                default:
-                    // TODO: Aqui certamente temos que mandar para uma página de erro!!! (nunca deverá acontecer)
-                    result = ok("BUUMMM");
-            }
-        }
-        return result;
+        return routing(
+                DashboardStudent.milestones(id_project),
+                DashboardTeacher.milestones(id_project),
+                TODO);
     }
 
     @Security.Authenticated(Secured.class)
     public static Result showMilestone(Long id_project, Long id_milestone) {
-        return TODO;
+        return routing(
+                DashboardStudent.showMilestone(id_project,id_milestone),
+                DashboardTeacher.showMilestone(id_project,id_milestone),
+                TODO);
     }
 
     @Security.Authenticated(Secured.class)
     public static Result createMilestone(Long id_project) {
-        return TODO;
+        return routing(
+                DashboardStudent.createMilestone(id_project),
+                DashboardTeacher.createMilestone(id_project),
+                TODO);
     }
 
     @Security.Authenticated(Secured.class)
     public static Result deleteMilestone(Long id_project, Long id_milestone) {
-        return TODO;
+        return routing(
+                DashboardStudent.deleteMilestone(id_project,id_milestone),
+                DashboardTeacher.deleteMilestone(id_project,id_milestone),
+                TODO);
     }
 
     @Security.Authenticated(Secured.class)
     public static Result avaliations(Long id_project,Long id_milestone) {
-        Result result = null;
-        User u = User.findByEmail(request().username());
-        if(u != null){
-            switch(u.userType){
-                case "Teacher":
-                    result =  DashboardTeacher.avaliations(id_project,id_milestone);
-                    break;
-                case "Student":
-                    result =  DashboardStudent.avaliations(id_project,id_milestone);
-                    break;
-                case "Administrator":
-                    break;
-                default:
-                    // TODO: Aqui certamente temos que mandar para uma página de erro!!! (nunca deverá acontecer)
-                    result = ok("BUUMMM");
-            }
-        }
-        return result;
+        return routing(
+                DashboardStudent.avaliations(id_project,id_milestone),
+                DashboardTeacher.avaliations(id_project,id_milestone),
+                TODO);
     }
 
     @Security.Authenticated(Secured.class)
     public static Result showAvaliation(Long id_project, Long id_milestone, Long id_avaliation) {
-        return TODO;
+        return routing(
+                DashboardStudent.showAvaliation(id_project,id_milestone,id_avaliation),
+                DashboardTeacher.showAvaliation(id_project,id_milestone,id_avaliation),
+                TODO);
     }
 
     @Security.Authenticated(Secured.class)
     public static Result createAvaliation(Long id_project,Long id_milestone) {
-        return TODO;
+        return routing(
+                DashboardStudent.createAvaliation(id_project,id_milestone),
+                DashboardTeacher.createAvaliation(id_project,id_milestone),
+                TODO);
     }
 
     @Security.Authenticated(Secured.class)
     public static Result deleteAvaliation(Long id_project,Long id_milestone, Long id_avaliation) {
-        return TODO;
+        return routing(
+                DashboardStudent.deleteAvaliation(id_project,id_milestone,id_avaliation),
+                DashboardTeacher.deleteAvaliation(id_project,id_milestone,id_avaliation),
+                TODO);
     }
 
 }
